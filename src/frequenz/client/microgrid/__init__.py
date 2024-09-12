52e81febff381af22705147e998f2bc8a2bdc6e5
Leandro Lucarella <luca-frequenz@llucax.com>
2024-09-12 15:47:14 +0200

    Remove previous implementation
    The API changed so much in v0.17 that building on top of the previous
    version will just add noise to the diff of the following commits, and
    we won't have checks passing for a long time either if we go this route
    because after upgrading the API bindings files nothings works anymore.
    
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- src/frequenz/client/microgrid/__init__.py
+++ src/frequenz/client/microgrid/__init__.py
@@ -8,38 +8,6 @@ This package provides a low-level interface for interacting with the microgrid A
 
 
 from ._client import MicrogridApiClient
-from ._component import (
-    Component,
-    ComponentCategory,
-    ComponentMetadata,
-    ComponentMetricId,
-    ComponentType,
-    Fuse,
-    GridMetadata,
-    InverterType,
-)
-from ._component_data import (
-    BatteryData,
-    ComponentData,
-    EVChargerData,
-    InverterData,
-    MeterData,
-)
-from ._component_error import (
-    BatteryError,
-    BatteryErrorCode,
-    ErrorLevel,
-    InverterError,
-    InverterErrorCode,
-)
-from ._component_states import (
-    BatteryComponentState,
-    BatteryRelayState,
-    EVChargerCableState,
-    EVChargerComponentState,
-    InverterComponentState,
-)
-from ._connection import Connection
 from ._exception import (
     ApiClientError,
     ClientNotConnected,
@@ -63,44 +31,17 @@ from ._exception import (
     UnrecognizedGrpcStatus,
 )
 from ._lifetime import Lifetime
-from ._metadata import Location, Metadata
 
 __all__ = [
     "ApiClientError",
-    "BatteryComponentState",
-    "BatteryData",
-    "BatteryError",
-    "BatteryErrorCode",
-    "BatteryRelayState",
     "ClientNotConnected",
-    "Component",
-    "ComponentCategory",
-    "ComponentData",
-    "ComponentMetadata",
-    "ComponentMetricId",
-    "ComponentType",
-    "Connection",
     "DataLoss",
-    "EVChargerCableState",
-    "EVChargerComponentState",
-    "EVChargerData",
     "EntityAlreadyExists",
     "EntityNotFound",
-    "ErrorLevel",
-    "Fuse",
-    "GridMetadata",
     "GrpcError",
     "InternalError",
     "InvalidArgument",
-    "InverterComponentState",
-    "InverterData",
-    "InverterError",
-    "InverterErrorCode",
-    "InverterType",
     "Lifetime",
-    "Location",
-    "Metadata",
-    "MeterData",
     "MicrogridApiClient",
     "OperationAborted",
     "OperationCancelled",
