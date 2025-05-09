e943fc685dd6c35d10fa54bbc5e0d37c9f78faf0
Leandro Lucarella <luca-frequenz@llucax.com>
2025-05-09 15:51:17 +0200

    Add a sensor ID wrapper
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- src/frequenz/client/microgrid/_id.py
+++ src/frequenz/client/microgrid/_id.py
@@ -1,7 +1,7 @@
 # License: MIT
 # Copyright © 2025 Frequenz Energy-as-a-Service GmbH
 
-"""Strongly typed IDs for microgrids and components."""
+"""Strongly typed IDs for microgrids, components and sensors."""
 
 
 from typing import final
@@ -105,3 +105,53 @@ class ComponentId:
     def __str__(self) -> str:
         """Return the short string representation of this instance."""
         return f"CID{self._id}"
+
+
+@final
+class SensorId:
+    """A unique identifier for a microgrid sensor."""
+
+    def __init__(self, id_: int, /) -> None:
+        """Initialize this instance.
+
+        Args:
+            id_: The numeric unique identifier of the microgrid sensor.
+
+        Raises:
+            ValueError: If the ID is negative.
+        """
+        if id_ < 0:
+            raise ValueError("Sensor ID can't be negative.")
+        self._id = id_
+
+    def __int__(self) -> int:
+        """Return the numeric ID of this instance."""
+        return self._id
+
+    def __eq__(self, other: object) -> bool:
+        """Check if this instance is equal to another object."""
+        # This is not an unidiomatic typecheck, that's an odd name for the check.
+        # isinstance() returns True for subclasses, which is not what we want here.
+        # pylint: disable-next=unidiomatic-typecheck
+        return type(other) is SensorId and self._id == other._id
+
+    def __lt__(self, other: object) -> bool:
+        """Check if this instance is less than another object."""
+        # pylint: disable-next=unidiomatic-typecheck
+        if type(other) is SensorId:
+            return self._id < other._id
+        return NotImplemented
+
+    def __hash__(self) -> int:
+        """Return the hash of this instance."""
+        # We include the class because we explicitly want to avoid the same ID to give
+        # the same hash for different classes of IDs
+        return hash((SensorId, self._id))
+
+    def __repr__(self) -> str:
+        """Return the string representation of this instance."""
+        return f"{type(self).__name__}({self._id!r})"
+
+    def __str__(self) -> str:
+        """Return the short string representation of this instance."""
+        return f"SID{self._id}"
