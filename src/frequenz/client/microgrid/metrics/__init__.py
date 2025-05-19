c3059707b2c313abe349e2e5a22ba2346e44edc6
Leandro Lucarella <luca-frequenz@llucax.com>
2025-05-19 11:57:23 +0200

    Move the `metrics` module to a package
    We will need to add more metrics-related wrappers in the future, so it
    is better to make it a package.
    
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

---
+++ src/frequenz/client/microgrid/metrics/__init__.py
@@ -0,0 +1,11 @@
+# License: MIT
+# Copyright © 2025 Frequenz Energy-as-a-Service GmbH
+
+"""Metrics definitions."""
+
+from ._sample import AggregatedMetricValue, AggregationMethod
+
+__all__ = [
+    "AggregatedMetricValue",
+    "AggregationMethod",
+]
