079f2db436dda3fec7ba85072bb0c97b85eeb64c
Leandro Lucarella <luca-frequenz@llucax.com>
2025-12-18 10:57:18 +0100

    Improve sensor module documentation
    Now that all symbols are defined, we can enhance the module-level
    documentation to provide a clearer overview of the package's purpose
    and the data structures it offers for handling sensor telemetry.
    
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- src/frequenz/client/microgrid/sensor/__init__.py
+++ src/frequenz/client/microgrid/sensor/__init__.py
@@ -1,11 +1,32 @@
 # License: MIT
 # Copyright © 2025 Frequenz Energy-as-a-Service GmbH
 
-"""Sensor types and utilities.
+"""Microgrid sensors.
 
-This module provides classes and utilities for working with sensors in a
-microgrid environment. Sensors measure various physical metrics in the
-surrounding environment, such as temperature, humidity, and solar irradiance.
+This package provides classes and utilities for working with different types of
+sensors in a microgrid environment. [`Sensor`][frequenz.client.microgrid.sensor.Sensor]s
+measure various physical metrics in the surrounding environment, such as temperature,
+humidity, and solar irradiance.
+
+# Sensor Telemetry
+
+This package also provides several data structures for handling sensor readings
+and states:
+
+* [`SensorTelemetry`][frequenz.client.microgrid.sensor.SensorTelemetry]:
+    Represents a collection of measurements and states from a sensor at a specific
+    point in time, including [metric
+    samples][frequenz.client.microgrid.metrics.MetricSample] and [state
+    snapshots][frequenz.client.microgrid.sensor.SensorStateSnapshot].
+* [`SensorStateSnapshot`][frequenz.client.microgrid.sensor.SensorStateSnapshot]:
+    Contains the sensor's state codes and any associated diagnostic information.
+* [`SensorDiagnostic`][frequenz.client.microgrid.sensor.SensorDiagnostic]:
+    Represents a diagnostic message from a sensor, including an error code and
+    optional additional information.
+* [`SensorDiagnosticCode`][frequenz.client.microgrid.sensor.SensorDiagnosticCode]:
+    Defines error codes that a sensor can report.
+* [`SensorStateCode`][frequenz.client.microgrid.sensor.SensorStateCode]:
+    Defines codes representing the operational state of a sensor.
 """
 
 from ._sensor import Sensor
